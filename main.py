8ab62cc988c4681abc29a2b5af8be31041d735aa
Francisco Andrade <francisco@inspr.com>
2021-07-26 21:14:58 -0300

    Finished refactored implementation

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 import numpy as np
 import sys
+from numpy import round
 import utils
-# page 32
-# inviability => page 41
+
 try:
-    matrix_size = list(map(lambda x: int(x), input().strip().split(" ")))
-    c_T = np.array(list(map(lambda x: int(x), input().strip().split(" "))))
-    rest_str = ""
-    for i in range(0, matrix_size[0]):
-        rest_str += input().strip()
-        if i < matrix_size[0]-1:
-            rest_str += ";"
-    restrictions = np.matrix(rest_str)
+    n_restr = n_vars = 0
+    c_T = []
+    restrictions = []
+    b = []
+    for idx, line in enumerate(sys.stdin):
+        if idx == 0:
+            n_restr, n_vars = map(int, line.split())
+        elif idx == 1:
+            c_T = np.array(list(map(int, line.split())))
+        else:
+            cur_line = list(map(int, line.split()))
+            restrictions += [np.array(cur_line[:-1])]
+            b += [cur_line[-1]]
+
 except:
     print("invalid input arguments")
     sys.exit(1)
 
-vero = utils.create_VERO(c_T, restrictions)
-# auxT_end = len(restrictions) - 1
-# tab_init = len(restrictions)
-# tab_end = np.size(vero, 1) - 2
-# b = np.size(vero, 1) - 1
-utils.check_and_resolve_negative_b(vero)
+vero = utils.create_VERO(c_T, np.matrix(restrictions), b)
+tabl = utils.return_vero_wout_auxtable(vero, n_restr)
 
-aux_lp = utils.create_aux_lp(restrictions)
-utils.check_and_resolve_negative_b(aux_lp)
+# total_vars represents the inputed variables + loose variables
+total_vars = n_vars + n_restr
+valid_base = utils.get_base(tabl, n_restr, total_vars)
 
-utils.simplex_aux_lp(matrix_size[0], aux_lp)
+result, opt_val, opt_base, cert = utils.solver(
+    vero, n_restr, total_vars, total_vars, valid_base)
+
+print(result)
+
+if result == "ilimitada":
+    last_cert = total_vars - n_restr
+else:
+    last_cert = n_restr
+
+if opt_val is not None:
+    print(f"{round(opt_val, 7):.7f}")
+
+if opt_base is not None:
+    for i in opt_base[:total_vars - n_restr]:
+        print(f"{round(i, 7):.7f}", end=' ')
+    print()
+
+if cert is not None:
+    for i in cert[:last_cert]:
+        print(f"{round(i, 7):.7f}", end=' ')
+    print()
--- utils.py
+++ utils.py
@@ -1,70 +1,138 @@
 import numpy as np
 
 
-def create_VERO(c_T, restr):
-    aux_table_row1 = [0] * len(restr)
-    c_T_zeroes = [0] * (len(restr) + 1)
+def create_VERO(c_T, restr, b):
+    aux_table_row1 = [0] * restr.shape[0]
+    c_T_zeroes = [0] * (restr.shape[0] + 1)
     # creates first row of VERO
     head = np.append(aux_table_row1, c_T)
     head = np.negative(np.append(head, c_T_zeroes))
     # creates identity matrix for VERO's auxiliar table
-    aux_table = np.identity(len(restr))
-    # puts restrictions in FPI
-    restr_FPI = format_FPI(restr)
-    # generate tableaux but it's first row
-    body = np.append(aux_table, restr_FPI, axis=1)
-    return np.insert(body, 0, head, axis=0)
+    # and loose restrictions
+    id_matrix = np.identity(restr.shape[0])
+    # appends identity in both sides of restrictions
+    body = np.append(id_matrix, restr, axis=1)
+    body = np.append(body, id_matrix, axis=1)
+    # appends b column as the last column
+    body = np.c_[body, np.matrix(b).T]
+
+    return np.asarray(np.insert(body, 0, head, axis=0))
 
 
-def format_FPI(restr):
-    id_matrix = np.identity(len(restr))
-    b = np.squeeze(np.asarray(restr[:, restr.shape[1] - 1]))
-    wout_b = np.delete(restr, restr.shape[1]-1, 1)
-    # adds loose variables before 'b' column
-    # fpi = np.insert(id_matrix, [len(wout_b)], wout_b, axis=1)
-    fpi = np.append(wout_b, id_matrix, axis=1)
-    return np.c_[fpi, b]
+def get_base(tabl, n_restr, n_vars):
+    c_T = tabl[0, :-1]
+    restr = tabl[1:, :-1]
+    base = np.zeros(n_restr)
+    current = 0
+    for i in range(0, n_vars):
+        if c_T[i] == 0 and restr[restr[:, i] == 1, current].shape[0] == 1:
+            base[current] = i
+            current += 1
+            if current == n_restr:
+                break
+
+    return base
 
 
-def check_and_resolve_negative_b(vero):
-    b = np.squeeze(np.asarray(vero[1:, vero.shape[1] - 1]))
-    for i in range(0, len(b)):
-        if b[i] < 0:
-            vero[i+1] = np.negative(vero[i+1])
+def solver(vero, n_restr, n_vars, n_costs, b):
+    # generates auxiliar linear programming
+    lp_aux = create_aux_lp(vero[1:, n_restr:])
+    lp_aux[0] -= lp_aux[1:].sum(axis=0)
+    # solves auxiliar linear programming with simplex
+    _, ot, __, cert, lp_aux, b = simplex(lp_aux, n_restr, n_vars +
+                                         n_restr, n_vars + n_restr, np.arange(n_restr) + n_vars)
+
+    # checks if optimum value of auxiliar lp is negative
+    if ot is not None and np.round(ot, 7) < 0:
+        return "inviavel", None, None, cert
+
+    tabl = return_vero_wout_auxtable(vero, n_restr)
+    # updates vero table with bases extracted from solving auxiliar lp
+    vero[:, :n_restr + n_vars] = lp_aux[:, :n_restr + n_vars]
+    vero[0, n_restr:] = tabl[0] + lp_aux[0, :n_restr].T@tabl[1:]
+    vero[:, -1] = lp_aux[:, -1]
+
+    for i in b:
+        pivot(vero, i + n_restr, np.arange(n_restr)
+              [vero[1:,  n_restr + i] != 0][0] + 1)
+
+    return simplex(vero, n_restr, n_vars, n_costs, b)[:-2]
+
+
+def simplex(vero, n_restr, n_vars, n_costs, b):
+    while True:
+        tabl = return_vero_wout_auxtable(vero, n_restr)
+
+        # checks if all elements in c_T are greater than zero
+        if (-tabl[0, :-1] <= 0).all():
+            return "otima", tabl[0, -1], optim_base(tabl, n_vars, b),  vero[0, :n_restr], vero, b
+
+        # gets index of next base candidate
+        nxt_base_candidate = (np.arange(n_costs)[tabl[0, :-1] < 0])[0]
+        # checks if all elements of selected row are negative
+        if (tabl[1:, nxt_base_candidate] <= 0).all():
+            cert = np.zeros(n_vars)
+            cert[b[b < n_vars]] = tabl[1:, nxt_base_candidate]
+            cert *= -1
+            cert[nxt_base_candidate] = 1
+            return "ilimitada", None,  optim_base(tabl, n_vars, b), cert[:n_vars], vero, b
+
+        row = tabl[1:, nxt_base_candidate]
+        indices = np.arange(row.shape[0])[row > 0] + 1
+        index = indices[np.argmin((tabl[indices, -1]/row[indices - 1]))]
+        current = tabl[index][b] != 0
+        b[current] = nxt_base_candidate
+
+        pivot(vero, nxt_base_candidate + n_restr, index)
+
+
+# Auxiliar functions
+
+
+def pivot(tabl, col, row):
+    tabl[row] /= tabl[row, col]
+
+    tabl[:row] -= (tabl[:row, col] / tabl[row, col]
+                   )[:, np.newaxis] * tabl[row, ]
+
+    tabl[row + 1:] -= (tabl[row + 1:, col] / tabl[row, col]
+                       )[:, np.newaxis] * tabl[row, ]
+
+
+def optim_base(tableaux, n_vars, b):
+    # mask to filter valid base values
+    mask = np.zeros(n_vars)
+    mask[b[b < n_vars]] = 1
+
+    base = np.zeros(n_vars)
+    for row in tableaux[1:]:
+        base += row[:n_vars] * mask * row[-1]
+
+    return base
+
+
+def return_vero_wout_auxtable(vero, n_restr):
+    vero_cpy = vero.copy()
+    return vero_cpy[:, n_restr:]
 
 
 def create_aux_lp(restr):
     id_matrix = np.identity(restr.shape[0])
-    head = np.append([0] * (restr.shape[1] - 1), [-1] * (restr.shape[0]))
+    head = np.append([0] * (restr.shape[1] - 1), [1] * (restr.shape[0]))
     head = np.append(head, [0])
     head = np.append([0]*restr.shape[0], head)
-    b = np.squeeze(np.asarray(restr[:, restr.shape[1] - 1]))
-    wout_b = np.delete(restr, restr.shape[1]-1, 1)
+    fpi = np.append(id_matrix, restr, axis=1)
+    # invert signals of lines that contain negative value for b
+    check_and_resolve_negative_b(fpi)
+    b = np.squeeze(np.asarray(fpi[:, fpi.shape[1] - 1]))
+    wout_b = np.delete(fpi, fpi.shape[1]-1, 1)
     fpi = np.append(wout_b, id_matrix, axis=1)
-    fpi = np.append(id_matrix, fpi, axis=1)
     fpi = np.c_[fpi, b]
-    return np.insert(fpi, 0, head, axis=0)
-
-# returns string, optimal value, xi_bi, certificate(aux tableax)
+    return np.asarray(np.insert(fpi, 0, head, axis=0))
 
 
-def simplex_aux_lp(n_restr, vero):
-    def wout_aux_table():
-        return vero[:, n_restr:]
-
-    while True:
-        if (wout_aux_table()[0, :-1] <= 0).all():
-            if wout_aux_table()[0, -1] == 0:
-                return viable_base(wout_aux_table())
-    # TODO: add inviability check
-    # TODO: see if LP Aux defines if main LP is unlimited
-
-
-def viable_base(n_restr, tabl):
-    base = [0] * tabl.shape[1] - 1
-    for i in range(0, tabl.shape[1]-1):
-        col_T = np.squeeze(tabl[:, i])
-        if np.count_nonzero(col_T) == 1 and len(np.where(col_T == 1)[1]) > 0:
-            base[np.where(col_T == 1)[1]] = tabl[:, -
-                                                 1][np.where(col_T == 1)[1]]
-    return base[:n_restr]
+def check_and_resolve_negative_b(tabl):
+    b = np.squeeze(np.asarray(tabl[:, tabl.shape[1] - 1]))
+    for i in range(0, len(b)):
+        if b[i] < 0:
+            tabl[i] = np.negative(tabl[i])
